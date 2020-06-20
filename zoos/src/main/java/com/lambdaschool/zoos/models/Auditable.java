aa0d7e67bf91568967572b531438544b68efd414
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-20 13:36:34 +0800

    Create audit fields

---
+++ zoos/src/main/java/com/lambdaschool/zoos/models/Auditable.java
@@ -0,0 +1,33 @@
+package com.lambdaschool.usermodel.zoos;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+abstract class Auditable {
+    @CreatedBy
+    protected String created_by;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date created_date;
+
+    @LastModifiedBy
+    protected String last_modified_by;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date last_modified_date;
+
+}
