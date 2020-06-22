488669bd70a4adff987ffc9b1f789629363b3f9d
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-22 20:24:57 +0800

    Revert "Add create/replace zoo functionality"
    This reverts commit ab83f71e

--- zoos/src/main/java/com/lambdaschool/zoos/services/ZooService.java
+++ zoos/src/main/java/com/lambdaschool/zoos/services/ZooService.java
@@ -22,4 +22,21 @@ public interface ZooService {
      * @return The Zoo object you seek
      */
     Zoo findZooById(long id);
+
+    /**
+     * Given a complete zoo object, saves that zoo object in the database.
+     * If a primary key is provided, the record is completely replaced
+     * If no primary key is provided, one is automatically generated and the record is added to the database.
+     *
+     * @param zoo the zoo object to be saved
+     * @return the saved zoo object including any automatically generated fields
+     */
+    Zoo save(Zoo zoo);
+
+    /**
+     * Adds an entry to the ZooAnimals join table
+     * @param zooid
+     * @param animalid
+     */
+    void addZooAnimal(long zooid, long animalid);
 }
