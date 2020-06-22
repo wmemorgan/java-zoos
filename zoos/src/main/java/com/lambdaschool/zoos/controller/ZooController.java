86d808a5089c51f2476874b9e3d81d359c5fa287
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-22 20:22:18 +0800

    Add GET '/zoos/zoo/{zooid}' route
    Add POST '/zoos/zoo' route

--- zoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
+++ zoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
@@ -3,15 +3,13 @@ package com.lambdaschool.zoos.controller;
 import com.lambdaschool.zoos.models.Zoo;
 import com.lambdaschool.zoos.services.ZooService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -38,46 +36,10 @@ public class ZooController {
         return new ResponseEntity<>(zooList, HttpStatus.OK);
     }
 
-    /**
-     * Returns a single user based off a user id number
-     * <br>Example: http://localhost:2019/zoos/zoo/{zooid}
-     *
-     * @param zooid The primary key of the user you seek
-     * @return JSON object of the user you seek
-     * @see ZooService#findZooById(long) (long) ZooService.findZooById(long)
-     */
-    @GetMapping(value = "/zoo/{zooid}", produces = {"application/json"})
+    @GetMapping(value = "/zoo/{zooid}", produces = "application/json")
     public ResponseEntity<?> findZooById(@PathVariable long zooid) {
         Zoo z = zooService.findZooById(zooid);
 
         return new ResponseEntity<>(z, HttpStatus.OK);
-    }
-
-    /**
-     * Given a complete Zoo Object, create a new User record and accompanying useremail records
-     * and user role records.
-     * <br> Example: <a href="http://localhost:2019/users/user">http://localhost:2019/users/user</a>
-     *
-     * @param newZoo A complete new user to add including emails and roles.
-     *                roles must already exist.
-     * @return A location header with the URI to the newly created user and a status of CREATED
-     * @throws URISyntaxException Exception if something does not work in creating the location header
-     * @see ZooService#save(Zoo) ZooService.save(Zoo)
-     */
-    @PostMapping(value = "/zoo", consumes = {"application/json"})
-    public ResponseEntity<?> addZoo(@Valid @RequestBody Zoo newZoo) {
-        newZoo.setZooid(0);
-        newZoo = zooService.save(newZoo);
-
-        // set the location header for the newly created resource
-        HttpHeaders responseHeaders = new HttpHeaders();
-        URI newZooURI = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{newzooid}")
-                .buildAndExpand(newZoo.getZooid())
-                .toUri();
-        responseHeaders.setLocation(newZooURI);
-
-        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
-
     }
 }
