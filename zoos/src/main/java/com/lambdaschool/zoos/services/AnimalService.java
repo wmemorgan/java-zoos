6a279c590716a5139a639a0128af14e21258f6ad
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-17 11:36:46 +0800

    Add custom database query

---
+++ zoos/src/main/java/com/lambdaschool/zoos/services/AnimalService.java
@@ -0,0 +1,10 @@
+package com.lambdaschool.zoos.services;
+
+import com.lambdaschool.zoos.views.AnimalCount;
+
+import java.util.List;
+
+public interface AnimalService {
+
+    List<AnimalCount> getAnimalCount();
+}
