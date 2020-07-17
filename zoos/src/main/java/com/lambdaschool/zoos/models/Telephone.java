795b37e582c2ecb243ebeae605e88cfdde0c261e
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-17 12:05:37 +0800

    Fix missing Constructor, Getters/Setters

--- zoos/src/main/java/com/lambdaschool/zoos/models/Telephone.java
+++ zoos/src/main/java/com/lambdaschool/zoos/models/Telephone.java
@@ -18,4 +18,46 @@ public class Telephone extends Auditable {
     @ManyToOne
     @JoinColumn(name = "zooid")
     private Zoo zoo;
+
+    public Telephone() {
+    }
+
+    public Telephone(long phoneid, String phonetype, String phonenumber, Zoo zoo) {
+        this.phoneid = phoneid;
+        this.phonetype = phonetype;
+        this.phonenumber = phonenumber;
+        this.zoo = zoo;
+    }
+
+    public long getPhoneid() {
+        return phoneid;
+    }
+
+    public void setPhoneid(long phoneid) {
+        this.phoneid = phoneid;
+    }
+
+    public String getPhonetype() {
+        return phonetype;
+    }
+
+    public void setPhonetype(String phonetype) {
+        this.phonetype = phonetype;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public Zoo getZoo() {
+        return zoo;
+    }
+
+    public void setZoo(Zoo zoo) {
+        this.zoo = zoo;
+    }
 }
